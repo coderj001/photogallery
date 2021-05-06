a749931a8fec39f2de84edb78686f9c626507d28
coderj001 <amirajubolchi001@gmail.com>
2021-05-06 21:47:21 +0530

    react base setup and create title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>PhotoGallery</h1>
+      <h2>Your Pictures</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua.{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Title;
